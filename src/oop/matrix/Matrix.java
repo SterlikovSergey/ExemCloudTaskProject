f87941cb5cf7a7f762f4f7831bcd16655c7a6208
sergeysterlikov <sergebyminsk@icloud.com>
2023-04-16 05:55:26 +0300

    Matrix intro

---
+++ src/oop/matrix/Matrix.java
@@ -0,0 +1,20 @@
+package matrix;
+
+public class Matrix {
+    private int[][] array;
+    private int numberOfRows;
+    private int numbersOfColumns;
+
+    public void additionByMatrix(int[] matrix1, int[] matrix2){
+
+    }
+    public void multiplicationByMatrix(){
+
+    }
+    public void multiplicationByNumber(int number){
+
+    }
+    public void printout(int[] matrix){
+
+    }
+}
---
+++ src/oop/matrix/MatrixMain.java
@@ -0,0 +1,7 @@
+package matrix;
+
+public class MatrixMain {
+    public static void main(String[] args) {
+
+    }
+}
